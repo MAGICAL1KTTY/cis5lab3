c446dfedbe8b4090de97ead7c6dfee9b547f7ead
MAGICAL1KTTY <181618833+MAGICAL1KTTY@users.noreply.github.com>
2026-07-08 22:39:02 -0700

    direction
    directions and some code written

---
+++ lab3/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+
+// set array w size 10 and have values range from 0-99
+// find sum & avg of array
+// output how many students will pass; score needs to be 70+
+// find the lowest and highest score in the array w/o sorting
+
+int main() {
+	int scores[] = {85, 10, 70, 80, 90, 98, 92, 60, 77, 2};
+
+	for (int i = 0; i < scores; i++) {
+		// find sum of array
+	}
+	double avg = sum / 10; // find avg of array
+
+}
