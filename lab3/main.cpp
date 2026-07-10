9658afa6ff22f40807ecfb2f8a54cda0122abfb3
MAGICAL1KTTY <181618833+MAGICAL1KTTY@users.noreply.github.com>
2026-07-10 11:48:03 -0700

    finished code
    proofread for stuff in case i missed anything

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -11,32 +11,32 @@ using std::string;
 //7 will pass, highest score is 90 (index 7),
 //lowest score is 2 (index 9)
 
-int main() {
+int main() 
+{
 	int scores[] = {85, 10, 70, 80, 90, 98, 92, 60, 77, 2};
 	int n = sizeof(scores) / sizeof(scores[0]); //finding size of array but unsure about using these commands
+	
 	int sum = 0;
-
-	for (int i = 0; i < 10; i++) // find sum of array
+	for (int i = 0; i < n; i++) // find sum of array
 	{
 		sum += scores[i];
 	}
 
 	double avg = sum / n; // find avg of array
-
 	int j = 0;
 	int p = 0;
-
-	while (j < 10){
+	while (j < n) {
 		if (scores[j] >= 70) // find how many students will pass
 		{
 			p++;
 		}
+		j++;
 	}
 	int maxScore = scores[0];
 	int maxIndex = 0;
-	int lowestScore = scores[0];
+	int lowestScore = 0;
 	int lowestIndex = 0;
-	for (int k = 1; k < 10; k++) // find lowest and highest score in array w/o sorting
+	for (int k = 1; k < n; k++) // find lowest and highest score in array w/o sorting
 	{
 		if (scores[k] > maxScore) {
 			maxScore = scores[k];
@@ -45,12 +45,12 @@ int main() {
 			lowestScore = scores[k];
 			lowestIndex = k;
 		}
+		k++;
 	}
 	cout << "Sum is: " << sum << endl;
-	cout << "Average is: " << fixed << setprecision(1) << avg << endl;
+	cout << "Average is: " << fixed << setprecision(2) << avg << endl;
 	cout << p << " will pass" << endl;
 	cout << "Highest score is: " << maxScore << endl;
 	cout << "Lowest score is: " << lowestScore << endl;
-
-
+	return 0;
 }
