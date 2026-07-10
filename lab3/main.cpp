174c04e039e7b442528c7ee40aff491bc0fcbc04
MAGICAL1KTTY <181618833+MAGICAL1KTTY@users.noreply.github.com>
2026-07-10 09:47:25 -0700

    program
    nothing is outputting to the console but all variables are defined, maybe i got stuck in a loop or something

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -1,17 +1,56 @@
 #include <iostream>
 #include <iomanip>
 
-// set array w size 10 and have values range from 0-99
-// find sum & avg of array
-// output how many students will pass; score needs to be 70+
-// find the lowest and highest score in the array w/o sorting
+using std::cout;
+using std::endl;
+using std::fixed;
+using std::setprecision;
+using std::string;
+
+//sum is 664, average is 66.4,
+//7 will pass, highest score is 90 (index 7),
+//lowest score is 2 (index 9)
 
 int main() {
 	int scores[] = {85, 10, 70, 80, 90, 98, 92, 60, 77, 2};
+	int n = sizeof(scores) / sizeof(scores[0]); //finding size of array but unsure about using these commands
+	int sum = 0;
 
-	for (int i = 0; i < scores; i++) {
-		// find sum of array
+	for (int i = 0; i < 10; i++) // find sum of array
+	{
+		sum += scores[i];
 	}
-	double avg = sum / 10; // find avg of array
+
+	double avg = sum / n; // find avg of array
+
+	int j = 0;
+	int p = 0;
+
+	while (j < 10){
+		if (scores[j] >= 70) // find how many students will pass
+		{
+			p++;
+		}
+	}
+	int maxScore = scores[0];
+	int maxIndex = 0;
+	int lowestScore = scores[0];
+	int lowestIndex = 0;
+	for (int k = 1; k < 10; k++) // find lowest and highest score in array w/o sorting
+	{
+		if (scores[k] > maxScore) {
+			maxScore = scores[k];
+			maxIndex = k;
+		} else {
+			lowestScore = scores[k];
+			lowestIndex = k;
+		}
+	}
+	cout << "Sum is: " << sum << endl;
+	cout << "Average is: " << fixed << setprecision(1) << avg << endl;
+	cout << p << " will pass" << endl;
+	cout << "Highest score is: " << maxScore << endl;
+	cout << "Lowest score is: " << lowestScore << endl;
+
 
 }
